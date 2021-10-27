5950aa8a39504d90eb00eea146deceaf5cab03e5
KaveyanHaran <93232966+KaveyanHaran@users.noreply.github.com>
2021-10-26 17:48:11 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,10,10)
+}
+
+function draw() 
+{
+  background(30);
+  if(keyDown("right")) {
+
+  box.x = box.x + 3
+
+  }
+  if(keyDown("left")){
+
+    box.x = box.x - 3
+
+  }
+  if (keyDown("up")){
+
+    box.y = box.y - 3
+
+  }
+  if (keyDown("down")){
+
+    box.y=box.y + 3
+    
+  }
+  drawSprites();
+}
+
+
+
+
